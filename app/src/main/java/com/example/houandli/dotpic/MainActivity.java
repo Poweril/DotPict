25112844c9aaa6ec4ad4e5165ee7a34440e35920
Poweril <chaselynli@gmail.com>
2017-09-16 16:47:20 +0800

    Upload.

---
+++ app/src/main/java/com/example/houandli/dotpic/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.houandli.dotpic;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+        public void newPic(View view) {
+            Intent intent = new Intent(this, activity_edit.class);
+            startActivity(intent);
+        }
+        /*      Alter
+        FloatingActionButton newPic = (FloatingActionButton)findViewById(R.id.newPic);
+        newPic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent editNew = new Intent(getApplicationContext(), activity_edit.class);
+                startActivity(editNew);
+            }
+        );*/
+}
